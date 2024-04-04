ae940ef53b8fce26798388e2ce8e08fc1006ae78
Angel <jose.prieto@levva.io>
2024-04-04 14:00:20 -0300

    feat: implement intl

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { useSelector } from "react-redux";
 import CartItemComponent from "./CartItemComponent";
 import { selectCartItems, selectCartTotal } from "../features/cart/cartSlice";
+import { FormattedNumber, useIntl } from "react-intl";
 
 const CartComponent: React.FC = () => {
+  const intl = useIntl();
   const cartItems = useSelector(selectCartItems);
   const total = useSelector(selectCartTotal);
 
+  const cartTitle = intl.formatMessage({ id: "cart" });
+
   return (
     <div>
-      <h1>Carrinho</h1>
+      <h1>{cartTitle}</h1>
       {cartItems.map((item) => (
         <CartItemComponent key={item.id} item={item} />
       ))}
       <div>
-        <strong>Total: R$ {total.toFixed(2)}</strong>
+        Total:{" "}
+        <FormattedNumber
+          value={+total.toFixed(2)}
+          style="currency"
+          currency="USD"
+        />
       </div>
     </div>
   );
